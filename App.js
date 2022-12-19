62ec7cc0f5e4952723ec5eb5fa196a6eadb6fd1c
Sheikh Areeb <67228908+Fighter15c@users.noreply.github.com>
2022-12-19 11:14:29 +0500

    first commit

--- App.js
+++ App.js
@@ -1,10 +1,65 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StatusBar } from "expo-status-bar";
+import { useEffect } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import {
+  AppOpenAd,
+  InterstitialAd,
+  RewardedAd,
+  BannerAd,
+  TestIds,
+  BannerAdSize,
+  AdEventType,
+  RewardedAdEventType,
+} from "react-native-google-mobile-ads";
+const iAdUnitId = TestIds.INTERSTITIAL;
+// const interstitial = InterstitialAd.createForAdRequest(iAdUnitId, {
+//   requestNonPersonalizedAdsOnly: true,
+//   keywords: ["fashion", "clothing"],
+// });
+const rAdUnitId = "ca-app-pub-6242860245790899/7005801633";
+const rewarded = RewardedAd.createForAdRequest(rAdUnitId, {
+  requestNonPersonalizedAdsOnly: true,
+  keywords: ["fashion", "clothing"],
+});
 export default function App() {
+  useEffect(() => {
+    const unsubscribeLoaded = rewarded.addAdEventListener(
+      RewardedAdEventType.LOADED,
+      () => {
+        console.log("Loaded ??");
+        rewarded.show();
+      }
+    );
+    const unsubscribeEarned = rewarded.addAdEventListener(
+      RewardedAdEventType.EARNED_REWARD,
+      (reward) => {
+        console.log("User earned reward of", reward);
+      }
+    );
+    rewarded.load();
+    return () => {
+      unsubscribeLoaded();
+      unsubscribeEarned();
+    };
+  });
+  // useEffect(() => {
+  //   const unsubscribe = interstitial.addAdEventListener(
+  //     AdEventType.LOADED,
+  //     () => {
+  //       console.log("Interstitial ad shown");
+  //       interstitial.show();
+  //     }
+  //   );
+  //   interstitial.load();
+  //   return unsubscribe;
+  // }, []);
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <BannerAd
+        size={BannerAdSize.BANNER}
+        unitId={"ca-app-pub-6242860245790899/7441163328"}
+      />
+      <Text>Open up Ap p.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +68,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
